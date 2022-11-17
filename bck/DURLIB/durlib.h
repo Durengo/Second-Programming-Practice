ec38098f7be2c387aab872c3dc19b8c5161f8d12
Sebastian <Sebastian.Termen@gmail.com>
2022-11-17 16:42:27 +0200

    derp

--- bck/DURLIB/durlib.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-//#include "durlib/Core/_CORE.h"
-#include "durlib/Log/Log.h"
-// #include "durlib/Datatype/Date/DateMath.h"
-#include "durlib/Input/Input.h"
-#include "durlib/Input/Validation.h"
-#include "durlib/Serialization/Json/JsonParse.h"
